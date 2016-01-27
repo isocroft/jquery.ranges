7489494892603758d1544becb65db322a4e7a961
Ifeora Okechukwu <isocroft@gmail.com>
2016-01-27 09:34:50 -0800

    latest updates for latest version 0.0.4

--- src/jquery.ranges.js
+++ src/jquery.ranges.js
@@ -1,5 +1,5 @@
 /**************************************************************************
- * Copyright © 2013 @cdv code labs All Rights Reserved.                   *
+ * Copyright © 2013-2016 @cdv code labs All Rights Reserved.              *
  *                                                                        *
  * @APIurl http://cproscodedev.com.ng/projects/js/ranges                  *
  * @licence:  MIT                                                         *
@@ -9,8 +9,8 @@
  * Author(s): Ifeora Okechukwu (@isocroft - http://twitter.com/isocroft)  *
  * Version: 0.0.4                                                         *
  * Date Created: 04/03/2013                                               *
- * Date Last Modified: 14/11/2015                                         *
- * Date Released: 00/00/0000                                              *
+ * Date Last Modified: 27/01/2016                                         *
+ * Last Released: 28/01/2016                                              *
  *                                                                        *
  * Redistribution and use in source and binary forms, with or without     *
  * modification, are permitted under open source licensing terms          *
@@ -31,7 +31,7 @@
      if(typeof define == "function" && "amd" in define){
 	      define(['jquery'], factory);
 	 }
-	 if(module && typeof module.exports == "object"){
+	 if(typeof module != "undefined" && typeof module.exports == "object"){
 	     factory(require('jquery'));
 	 }
 	 if(this.window === this && this.jQuery){
@@ -44,17 +44,21 @@
   
   $.fn.ranges = function(options){
    
-      
-    if($.rangeSet !== undefined){
-	       return;
-	  }
-
+   
     if(!$.isPlainObject(options)){
-        options = {};
-        $.rangeSet = false;
-		} 
+        if($(this.selector).data("rangeSet")){ // we have initialized before...
+		    if(typeof options == "string"){
+			     var secondArg = (arguments[1] != null || typeof arguments[1] != "undefined")? arguments[1] : "";
+				 return $(this.selector).prop(options, secondArg);
+			}else{
+			    throw new TypeError("invalid first argument to ranges object");
+			}
+		}else{
+		    options = {};
+		}
+    }
 		 
-	   
+	
 	 
 	   var __defaults = {
 	                     step:1,
@@ -82,9 +86,8 @@
 		            options = (($opts.vals >= $opts.min && $opts.max >= $opts.vals) && (diff % $opts.step == 0) && $opts) || null,
 		 
 		 $opts = null;
-		 
-		 $.rangeSet = true;	 // cache the fact that we have indeed initialised    	 	   
-		   
+		 	   	 	   
+		 $(this.selector).data("rangeSet", false);   
    
          return $(this).each(function(index){
 	   
@@ -96,8 +99,12 @@
                  $view = $('<div>').attr("class",'ranges-input-'+options.boxClass),			 
 				 $widget = $(this).attr('role','application').css({position:"relative"}),
 				 $wrapper_id = "aria-ranges-"+($widget[0].id || "widget"),
-				 $wrapper = $('<div id='+$wrapper_id+' class="ranges-wrapper"></div>'),                 
-                 wid = parseInt($widget.css("width")), 
+				 $wrapper = $('<div id='+$wrapper_id+' class="ranges-wrapper"></div>');
+				 
+				 $widget.data("rangeSet", true); // cache the fact that we have indeed initialised  
+                 $widget.wrap($wrapper);
+                 
+           var   wid = parseInt($widget.css("width")), 
                  hgt = parseInt($widget.css("height")), 
 				 widgOffsetWidth = $widget.outerWidth(),
                  qtrwidth = (0.25 * wid),
@@ -109,24 +116,22 @@
 				 pinHalfEffectiveWidth = 0.5*(parseInt(pinheight)),
 				 left = pxwid - pinHalfEffectiveWidth, 
 				 $pin = $('<a>').css({width:(pinheight-2)+"px",height:(pinheight-2)+"px",display:"block",textDecoration:"none",position:"absolute",left:left+"px",top:"0px",margin:"0px",padding:"0px",border:"1px solid transparent"}),
-				 $slidr = $('<div>').css({height:hlfheight+"px",position:"relative",padding:"0px",margin:qtrheight+"px 0px "+qtrheight+"px 0px",width:((parseInt($pin.css('left')) + pinHalfEffectiveWidth))+"px",border:"none"}),
+				 $slidr = $('<div>').css({height:hlfheight+"px",position:"relative",padding:"0px",margin:qtrheight+"px 0px "+qtrheight+"px 0px",width:((parseInt($pin.css('left')) + pinHalfEffectiveWidth ))+"px",border:"none"}),
 				 $arrw = $('<span>'),
 				 minLeft = (0 - pinHalfEffectiveWidth),
 				 maxLeft = (minLeft + wid),
 				 setObjPoints = function($jq1, $jq2, offset){    
-					 left = ((offset) <= minLeft)? minLeft : ((offset >= maxLeft) ? maxLeft : offset); /* include step functionality here later */
+				     left = ((offset) <= minLeft)? minLeft : ((offset >= maxLeft) ? maxLeft : offset-pinHalfEffectiveWidth); /* include step functionality here later */
 					 value = ((left + pinHalfEffectiveWidth) / wid) * diff;
-					 $slidr.attr('aria-value', value); 
+					 $jq2.attr('aria-value', value); 
 					 ratio = (value/diff) * 100;
 		             $jq1.css('left', left+"px");
 					 $jq2.css('width',(left+pinHalfEffectiveWidth)+"px");
 					 $('input.ranges-input-'+options.inputClass).val((options.roundValues)? Math.round(options.scale * value) : options.scale * value);
-					 
+					 return value; 
 		         };
 				 
-				 
 				 $view.html('<span class="ranges-input-'+options.inputTagClass+'"></span><input type="text" value="'+((options.roundValues)? Math.round(options.scale * value) : options.scale * value) +'" '+(options.inputEditable? 'readonly="readonly"': '')+' tabindex='+index+' class="ranges-input-'+options.inputClass+'" />');
-                 
 				 
 				 $arrw.addClass('arrow-ranges').addClass('arrow-ranges-n');
 				 $arrw.css({color:'#666666',borderWidth:options.arrowSize+'px',borderBottomColor:'#999999',borderBottomStyle:'solid',borderBottomWidth:options.arrowSize+'px',left:(pinHalfEffectiveWidth - 10)+"px"})
@@ -148,29 +153,43 @@
 				 $frag.appendChild($pin[0]); 
                  $widget[0].appendChild($frag);
 				 
-				 $widget.wrap($wrapper);
-				 $('.ranges-wrapper').append($view);
+				 $($widget[0].parentNode).append($view);
 				 
-		
-				 
-				 $pin.on('mousedown', function(e){	
-				       var t = e.clientX,
-					   handler = function(e){
-					            $(document).trigger('pindragon');
-            	                setObjPoints($pin, $slidr, (e.clientX-pinHalfEffectiveWidth)); 									 
-            	       };				   
+				 if(!('vals' in $.propHooks)){
+					 $.propHooks.vals = { // setting and getting slider value...
+						get:function(elem){
+						   var $slid = $(elem).find('[role="slider"]').eq(0);
+						   return parseFloat($slid.attr('aria-value'));
+						},
+						set:function(elem, val){
+						    val = parseInt(val) || 0;
+							var $pn = $(elem).find('[role="pin"]').eq(0);
+							var $slid = $(elem).find('[role="slider"]').eq(0);
+							setObjPoints($pn, $slid, (val-$pin.width()));
+						}
+					 };
+				 }	 
+					 
+		         $pin.on('mousedown', function(e){	
+				       var handler = function(e){
+					            var _event = $.Event( "pindragon" ), _offset = (e.clientX-$pin.width());
+								_event.value = setObjPoints($pin, $slidr, _offset); // [_offset] will constantly center the [$pin] underneath the mouse pointer!
+								$(document).trigger(_event);								
+            	       }, event = $.Event( "pindragstart" );		
+                        					   
 	                   if(!drag_active) drag_active = true;
-               x = this;
-					     left = parseInt($(x).css('left'));
-				       x.ondragstart = function(){ return false;  }; // inhibit (Firefox, IE) drag 'n drop wahala!!!
-				       if(drag_active){
-					          $(document).trigger('pindragstart');
-				              if($.throttle){ // if Ben Alman's --throttle & debounce-- is available to jQuery, then use it
-                                   $(document).bind('mousemove.rangespl', $.throttle(10, true, handler)); 
-					          }else{
-					               $(document).bind('mousemove.rangespl', function(ev){  handler(ev); }); // namespace the event to avoid conflict with other plugins
-					          }
-					    }
+                            x = this;
+					        left = parseInt($(x).css('left'));
+				            x.ondragstart = function(){ return false;  }; // inhibit (Firefox, IE) drag 'n drop wahala!!!
+								if(drag_active){
+								    event.value = $widget.prop('vals');
+									$(document).trigger(event);
+									if($.throttle){ // if Ben Alman's --throttle & debounce-- is available to jQuery, then use it
+										 $(document).bind('mousemove.rangespl', $.throttle(10, true, handler)); 
+									}else{
+										 $(document).bind('mousemove.rangespl', function(ev){  handler(ev); }); // namespace the event to avoid conflict with other plugins
+									}
+								}
                        			
 				});
 				
